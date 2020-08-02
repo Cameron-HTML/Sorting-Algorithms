9d4a1d39fae4c1227b887c8cae594ac498a4e10a
Cameron <cameron.242325@nv.ccsd.net>
2020-08-02 12:52:13 -0700

    Change scope of variable

--- BogoSort.js
+++ BogoSort.js
@@ -1,39 +1,38 @@
 // WANTED BALLS IN LIST
-var wantedBalls = 10;
+var wantedBalls = 5;
 
 var sorts = 0;
 
-var balls = []
-
 function insertBalls(wantedBalls) {
+	var balls = [];
+
 	for(i = 0; i < wantedBalls; i++) {
 		balls.push(i + 1);
 	}
 
-	shuffle();
+	shuffle(balls);
 }
 
-function shuffle() {
-	var preLength = balls.length
+function shuffle(array) {
+	var preLength = array.length
 	var sortedBalls = [];
 
 	for(i = 0; i < preLength; i++) {
-		var randomBall = Math.floor(Math.random() * balls.length + 1);
-		sortedBalls[i] = balls[randomBall - 1]
+		var randomBall = Math.floor(Math.random() * array.length + 1);
+		sortedBalls[i] = array[randomBall - 1]
 
-		balls.splice(randomBall - 1, 1);
+		array.splice(randomBall - 1, 1);
 	}
 
 	try {
-		//sort(sortedBalls, preLength - 1);
-		setTimeout(sort, 10, sortedBalls, preLength - 1);
+		sort(sortedBalls, preLength - 1);
+		//setTimeout(sort, 2, sortedBalls, preLength - 1);
 	} catch(error) {
 		console.log(error);
 	}
 }
 
 function sort(array, length) {
-	//Check if sorted
 	for(i = 0; i < length; i++) {
 		if(array[i] < array[i+1]) {
 			if(i >= length - 1) {
